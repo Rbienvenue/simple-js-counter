95d4c3aaf1791c2a983dc99e699e8017776a8894
Rudasingwa Dev <163637025+Rbienvenue@users.noreply.github.com>
2024-07-16 09:49:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function incre() {
+    let a=parseInt(document.getElementById("res").value);
+    a++
+    document.getElementById("res").value= a;
+}
+function decre() {
+    let a=parseInt(document.getElementById("res").value);
+    a--
+    document.getElementById("res").value= a;
+}
